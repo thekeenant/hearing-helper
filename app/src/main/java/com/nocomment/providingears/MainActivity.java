58f0169fedc806c6596da7ab8f8fca1beeedfe99
Joseph Fedor <joeyfedor@gmail.com>
2015-10-25 05:26:01 -0500

    removed activities and condesnsed to main activity

--- app/src/main/java/com/nocomment/providingears/MainActivity.java
+++ app/src/main/java/com/nocomment/providingears/MainActivity.java
@@ -1,14 +1,18 @@
 package com.nocomment.providingears;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.text.Spannable;
 import android.text.SpannableString;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -16,14 +20,57 @@ import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
+    private boolean isRuning = true;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         getActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor("#54DEFD")));
         Spannable text = new SpannableString("TEST FONT");
-        text.setSpan(new ForegroundColorSpan(Color.WHITE), 0, "test".length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+        text.setSpan(new ForegroundColorSpan(Color.WHITE), 0, "test" .length(), Spannable.SPAN_INCLUSIVE_INCLUSIVE);
         getActionBar().setTitle(text);
+
+
+        new LongOperation().execute("");
+    }
+
+    private class LongOperation extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPreExecute() {
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            NoiseRecorder noiseRecorder = new NoiseRecorder();
+            double db;
+
+            while (isRuning) {
+                db = noiseRecorder.highestNoiseLevel();
+                if (db > 90) {
+                    Vibrator v = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                    v.vibrate(500);
+                }
+                Log.d("NOISE LEVEL", String.valueOf(db));
+            }
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+        }
+
+        @Override
+        protected void onProgressUpdate(Void... values) {
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        isRuning = false;
     }
 
     @Override
@@ -48,15 +95,7 @@ public class MainActivity extends Activity {
         return super.onOptionsItemSelected(item);
     }
 
-    public void onAudioClick(View view){
-        startActivity(new Intent(getApplicationContext(), Audio.class));
-    }
-
-    public void onTranslateClick(View view){
+    public void onTranslateClick(View view) {
         startActivity(new Intent(getApplicationContext(), Translate.class));
-    }
-
-    public void onWeatherClick(View view){
-        startActivity(new Intent(getApplicationContext(), Weather.class));
     }
 }
